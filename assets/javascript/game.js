b42eecef0aa42d3805bb04e854541f5ec570a5df
Netcore89 <ernes.rave@gmail.com>
2018-11-03 15:26:49 -0600

    upload02

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,59 @@
-let randomNumber = Math.floor((Math.random() ))
+
+$(document).ready(function () {
+    var crystals = new Array(4);
+    var losses = "";
+    var wins = "";
+    var totalScore = 0;
+    var crystal = 0;
+    let value = 0;
+    let randomNumber = 0;
+
+    function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    function reset() {
+        
+        totalScore = 0;
+
+        randomNumber = getRandomArbitrary(19, 120);
+
+        for (let i = 0; i < crystals.length; i++) {
+            crystals[i] = getRandomArbitrary(1, 12);
+        }
+
+        /* crystals[0] = getRandomArbitrary(1, 12);
+        crystals[1] = getRandomArbitrary(1, 12);
+        crystals[2] = getRandomArbitrary(1, 12);
+        crystals[3] = getRandomArbitrary(1, 12);
+    
+        crystals = [ getRandomArbitrary(1, 12),  getRandomArbitrary(1, 12),  getRandomArbitrary(1, 12),  getRandomArbitrary(1, 12)]; */
+
+        console.log(crystals);
+
+        $("#randomNumber").text(randomNumber);
+    }
+    
+    reset();
+
+    $("div.buttons").children().on("click", function () {
+        //crystal = $(this).attr('id');
+        crystal = this.id;
+        value = crystals[crystal];
+        totalScore += value;
+        $("#totalScore").text(totalScore);
+
+        if (totalScore == randomNumber) {
+            wins++;
+            reset();
+            $("#wins").text(wins);
+        }
+        else if (totalScore > randomNumber) {
+            losses++;
+            $("#losses").text(losses);
+            reset();
+        }
+
+    });
+
+});
